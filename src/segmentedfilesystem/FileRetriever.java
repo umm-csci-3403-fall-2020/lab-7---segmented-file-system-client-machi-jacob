28704414793f2e5797d0172cbc937f78e1eccb16
JacobJenness <jacobsjenness@gmail.com>
2020-10-27 11:02:37 -0500

    Update Main and FileRetriever

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -1,13 +1,26 @@
 package segmentedfilesystem;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class FileRetriever {
 
-	public FileRetriever(String server, int port) {
+	InetAddress server;
+	int port;
+
+	public FileRetriever(String server, int port) throws java.net.UnknownHostException {
         // Save the server and port for use in `downloadFiles()`
         //...
+		if (server.equals("localhost")){
+			this.server = InetAddress.getLocalHost();
+		} else {
+			this.server = InetAddress.getByName(server);
+		}
+		this.port = port;
 	}
 
-	public void downloadFiles() {
+	public void downloadFiles() throws java.io.IOException{
         // Do all the heavy lifting here.
         // This should
         //   * Connect to the server
@@ -20,6 +33,14 @@ public class FileRetriever {
         // PacketManager.allPacketsReceived() that you could
         // call for that, but there are a bunch of possible
         // ways.
+
+		// Send an empty packet to the server.
+		byte[] sendBuf = new byte[256];
+		DatagramPacket packet = new DatagramPacket(sendBuf, sendBuf.length, server, port);
+		DatagramSocket socket = new DatagramSocket(port, server);
+		socket.send(packet);
+
+
 	}
 
 }
