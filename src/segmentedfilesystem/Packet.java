14cba50795b4255266f8958dcb088e6aa3c5a1a1
JacobJenness <jacobsjenness@gmail.com>
2020-11-01 15:55:35 -0600

    Finish Packet
    This commit updates the Packet class by adding a some comments to each method, as well as finishing some methods.

--- src/segmentedfilesystem/Packet.java
+++ src/segmentedfilesystem/Packet.java
@@ -5,21 +5,26 @@ public class Packet {
 
     byte[] data;
     int length;
+    boolean header;
+
     public Packet(byte[] data, int length){
         this.length = length;
         this.data = data;
     }
 
+    //Gets the status byte of the packet
     public byte getStatusByte(){
         byte status = data[0];//First byte
         return status;
     }
 
+    //Gets the file ID of the packet
     public byte getFileID(){
         byte fileID = data[1];//Second byte
         return fileID;
     }
 
+    //Gets the packet number of the packet
     //Only call if the packet is a data packet
     public int getPacketNum(){
         byte first = data[2];//Third byte
@@ -30,15 +35,20 @@ public class Packet {
         return packetNum;
     }
 
+    //Checks if the packet is a header packet or not
+    //Sets the boolean 'header' to true if true, and false if not
     public boolean isHeader(){
         int statusByte = Byte.toUnsignedInt(data[0]);
         if (statusByte % 2 == 0){
+            this.header = true;
             return true;
         } else {
+            this.header = false;
             return false;
         }
     }
 
+    //Checks if the packet is the last packet for the file
     public boolean isLastPacket(){
         int statusByte = Byte.toUnsignedInt(data[0]);
         if (statusByte  == 3%4 ){
@@ -48,14 +58,26 @@ public class Packet {
         }
     }
 
-    public byte getFileName(){
-        byte fileName = 0;
-        return fileName;
+    //Gets the file name from the header packet
+    public byte[] getFileName(){
+        if (header){ //Will not work if it isn't a header packet
+            byte[] fileName = new byte[length-2];
+            for (int i = 2; i<length;i++)
+                fileName[i-2] = data[i];
+            return fileName;
+        } else
+            return null;
     }
 
-    public byte getPacketData(){
-        byte packetData = 0;
-        return packetData;
+    //Gets the packet data from the data packet
+    public byte[] getPacketData(){
+        if (!header){
+            byte[] packetData = new byte[length-4];
+            for (int i = 4; i<length; i++)
+                packetData[i-4] = data[i];
+            return packetData;
+        } else
+            return null;
     }
 
 }
